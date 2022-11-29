b91fbafde3073f0cdcef61f60bd248d38d53d38d
sriram14a <114244306+sriram14a@users.noreply.github.com>
2022-11-29 12:19:50 +0530

    Add files via upload

---
+++ HPotter/script.js
@@ -0,0 +1,39 @@
+const characterList = document.getElementById('characters');
+const searchBar = document.getElementById('search')
+let res = [];
+
+searchBar.addEventListener('keyup',(e)=>{
+    const searchName = e.target.value
+      const filtered = res.filter((char)=>{ return (char.name.toLowerCase().includes(searchName)); })
+      display(filtered)
+})
+async function getchar() {
+
+    let url = await fetch("https://hp-api.herokuapp.com/api/characters")
+    res = await url.json();
+    console.log(res);
+    display(res)
+}
+
+
+function display(data) {
+    let characters = data.map((data) => {
+        return `
+          <li class=" row character-data mb-5">
+             <div class="img-div">
+             <img src="${data.image}" class="img-wrap" >
+              </div>
+                
+                <div class="text-center col-12 col-sm-6  ">
+                <h3>Name : ${data.name}</h5>
+                <p >House : ${data.house}</p> 
+                </div> 
+          </li>`
+
+    })
+        .join('')
+    characterList.innerHTML = characters;
+}
+getchar()
+
+
